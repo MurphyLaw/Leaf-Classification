05306e31588144f1714ad3da6b07bfd0783da982
Anthony Louise Balon <anthony.balon15@gmail.com>
2019-06-04 17:12:01 +0800

    added updates and android app

--- README.md
+++ README.md
@@ -5,29 +5,8 @@
 
 * Python 3.7
 * Install pip
-* pip install tensorflow
-* pip install keras
-* pip install sklearn
-* pip install matplotlib
-* pip install imutils
-* pip install numpy
-* pip install argparse
-* pip install random
-* pip install pickle
-* pip install cv2
-* pip install os
-* pip install kivy
-
-### Training
-
-execute 'python train.py -d dataset -m leaf.model -l lb.pickle'
-
-### Classification
-
-execute 'python train.py -m leaf.model -l lb.pickle -i examples/Bayabas_1.jpg'
+* pip install -r requirements.txt
 
 ### Kivy App
-
-comment out line code from 77 to 149 and uncomment from 1 to 74
 
 Then execute 'python main.py'
--- main.py
+++ main.py
@@ -8,10 +8,74 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import ObjectProperty
 from kivy.factory import Factory
 from kivy.uix.image import Image
-from pyimagesearch.classify import ClassifyImage
+from kivy.clock import Clock
+from kivy.network.urlrequest import UrlRequest
+# from pyimagesearch.classify import ClassifyImage
 import argparse
+from flask import Flask, jsonify, render_template, Response
+from threading import Thread
+from logging import Logger
+import cv2
+import time
+from camera_opencv import Camera
+
+from mjpegviewer import MjpegViewer
 
 kivy.require("1.10.1")
+app = Flask(__name__)
+
+scanning = False
+source = ""
+
+leafs = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/setting', methods=['GET'])
+def set_scanning():
+	global scanning
+	scanning = True
+	return sourceChecker()
+
+@app.route('/<int:leaf_id>', methods=['GET'])
+def get_tasks(leaf_id):
+	global scanning
+	global source
+	global leafs
+	if scanning:
+		source = leafs.get(leaf_id, "Not in Model")
+	return ''
+
+def sourceChecker():
+	global source
+	global scanning
+	while source == "" and scanning:
+		time.sleep(2)
+	return source
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
@@ -24,6 +88,14 @@ class ConnectPage(FloatLayout):
 	source = ""
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
+		self.add_widget(self.start_camera())
+
+	def start_camera(self):
+		viewer = MjpegViewer(
+		    url=
+		    "http://localhost:5000/video_feed")
+		viewer.start()
+		return viewer
 
 	def findImage_button(self):
 		self.show_load("Image")
@@ -38,9 +110,10 @@ class ConnectPage(FloatLayout):
 		self.data.update(information)
 
 	def classify_button(self):
-		label = ClassifyImage(self.data)
-		self.ids.labelclassification.text = label
-		print("[INFO] {}".format(label))
+		self.ids.labelclassification.text = "Scanning..."
+		def got_json(req, result):
+			self.ids.labelclassification.text = result
+		UrlRequest('http://localhost:5000/setting', got_json)
 
 	def dismiss_popup(self):
 		self._popup.dismiss()
@@ -50,6 +123,13 @@ class ConnectPage(FloatLayout):
 		self._popup = Popup(title=title, content=content,
 		                    size_hint=(0.9, 0.9))
 		self._popup.open()
+
+	def reset_button(self):
+		global scanning
+		global source
+		scanning = False
+		source = ""
+		self.ids.labelclassification.text = ""
 
 	def load(self, path, filename):
 		if filename[0].find(".model") > 0:
@@ -67,8 +147,20 @@ class ConnectPage(FloatLayout):
 			self.ids.image.source = filename[0]
 		self.dismiss_popup()
 
+
 class EpicApp(App):
-	pass
+	def build(self):
+		server = FlaskThread()
+		server.daemon = True
+		try:
+			server.start()
+		except (KeyboardInterrupt, SystemExit):
+			sys.exit()
+
+class FlaskThread(Thread):
+	def run(self):
+		Logger.manager.loggerDict['werkzeug'] = Logger.manager.loggerDict['kivy']
+		app.run(host='0.0.0.0')
 
 Factory.register('ConnectPage', cls=ConnectPage)
 
